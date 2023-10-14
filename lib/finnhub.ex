a297afe3639542dae7b277917e9d763cad36f0e4
Damien Henken <dhenken@gmail.com>
2023-10-14 00:56:03 -0500

    init

---
+++ lib/finnhub.ex
@@ -0,0 +1,18 @@
+defmodule Finnhub do
+  @moduledoc """
+  Documentation for `Finnhub`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Finnhub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/finnhub_test.exs
@@ -0,0 +1,8 @@
+defmodule FinnhubTest do
+  use ExUnit.Case
+  doctest Finnhub
+
+  test "greets the world" do
+    assert Finnhub.hello() == :world
+  end
+end
